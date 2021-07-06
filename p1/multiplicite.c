8fade0e1d4ce745d4ae0ecb36abbb775e53daaac
lucamat12113 <colosus6015@gmail.com>
2021-07-06 13:02:39 +0200

    euh je crois que c'est bon pour le main

--- p1/multiplicite.c
+++ p1/multiplicite.c
@@ -3,4 +3,27 @@
 
 int multiplicite(int *T, const int N,  int *max){
    
+    int i = 0;
+    int nbr = 1;
+    max = -2147483648; // le minimum pour un entier 
+
+    while(i<N){
+
+        if(T[i]>max){
+
+            max = T[i];
+            nbr = 1;
+
+        }else if(T[i]==max){
+
+            nbr++;
+
+        }
+
+        i++;
+
+    }
+
+   return nbr;
+
 }
