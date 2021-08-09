dcfb199c9c7a59cfa9760f5ac3f5fe8249674bb7
lucamat12113 <colosus6015@gmail.com>
2021-08-09 14:18:13 +0200

    quasi fiini

--- p2/course_tableau_test.c
+++ p2/course_tableau_test.c
@@ -1,2 +1,33 @@
+#include <assert.h>
+#include <stdlib.h>
+
 #include "course.h"
 #include "escale.h"
+
+
+static void print(Course *race){
+    assert(race!=NULL);
+    
+    int i;
+    Escale *step;
+    
+    for(i=0; i<how_many_escale(race); i++){
+        step = (Escale *)get(race);
+        
+    }//end for - i
+}//end print()
+
+int main(){
+
+    Escale *step1 = creation(50.63, 5.58, "Liège");
+    Escale *step2 = creation(50.56, 4.69, "Gembloux");
+
+    Course *race = create(step1, step2);
+
+    Escale *step3 = creation(50.83, 4.39, "Bruxelles");
+
+    race = add_step(race, 3, step3);
+    
+    return 1;
+    
+}//end main()
