d2e112f0d5b9ddf766962bd7b863bb102f64157b
lucamat12113 <colosus6015@gmail.com>
2021-08-15 10:24:37 +0200

    ok

--- Projet2/code/escale.c
+++ Projet2/code/escale.c
@@ -1,12 +1,3 @@
-/**
- * \file course.h
- * \brief ADT for managing the 'Course' type
- * \author Luca Matagne -- Université de Liège (ULg)
- * \date 16/07/2021
- *
- * Presents a 'course' (race) as an abstract data type
- */
-
 #include <stdlib.h>
 #include <assert.h>
 #include <math.h>
