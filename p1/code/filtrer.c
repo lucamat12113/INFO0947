8d79c91366ba9ab36cd05cb9f6bf5b795d209bab
lucamat12113 <colosus6015@gmail.com>
2022-03-30 09:54:21 +0200

    euh ouai

--- p1/code/filtrer.c
+++ p1/code/filtrer.c
@@ -10,16 +10,16 @@ int filtrer(int *T, int N){
 
     int i= 0;
     int taille_utile = 0;
-    int first_zero= 0;
+    //int first_zero= 0;
 
     while(i<N){
         if(!test(T[i])){
             T[i]= 0;
             i++;
-            first_zero= 1;
+            //first_zero= 1;
 
         }else{
-            if(first_zero!= 0){
+            if(taille_utile!= i){
                 T[taille_utile]= T[i];
                 T[i]= 0;
                 i++;
