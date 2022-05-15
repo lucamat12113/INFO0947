8d3a702c9e4fd1a5ba3ff4652a5e0f7155dea880
lucamat12113 <colosus6015@gmail.com>
2022-05-15 18:38:46 +0200

    point2d OK

--- p2/code/main.c
+++ p2/code/main.c
@@ -7,7 +7,7 @@ int main(){
 
 
     Point2D *a= Create(1.0,1.0);
-    Point2D *b= Create(2,2);
+    Point2D *b= Create(-5,-8);
     Point2D *c= Create(3,3);
 
     float gxa = get_x(a);
@@ -21,11 +21,15 @@ int main(){
 
     float dist = EuclDist(a,b);
 
+    Point2D* translation = TranslatePoint2D(a, c);
+
     printf("a = ( %f, %f)\n", gxa, gya );
     printf("b = ( %f, %f)\n", gxb, gyb );
     printf("c = ( %f, %f)\n", gxc, gyc );
 
     printf("distance = %f \n", dist);
+    printf("transl = %f, %f\n", get_x(TranslatePoint2D(a, b)), get_y(TranslatePoint2D(a, b)));
+    printf("rot = %f, %f\n", get_x(RotatePoint2D(a, c, 30)), get_y(RotatePoint2D(a, c, 30)));
 
 
     return 0;
